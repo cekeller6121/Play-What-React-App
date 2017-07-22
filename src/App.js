eecad7f44885f940c05de48486f73119354e2e4f
chadwick <cekeller6121@yahoo.com>
2017-07-22 10:22:37 -0400

    add files, start project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import NavBar from './components/NavBar.js';
+import PlayList from './components/PlayList.js';
+import PlayListForm from './components/PlayListForm.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <div><NavBar /></div>
+        <div><PlayList /></div>
+        <div><PlayListForm /></div>
+      </div>
+    );
+  }
+}
+
+export default App;
