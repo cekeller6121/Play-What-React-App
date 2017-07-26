ec8571a334329f64cf42492452ef1ce71f7c3f61
chadwick <cekeller6121@yahoo.com>
2017-07-26 11:23:54 -0400

    started .map build to render song data

--- src/components/PlayListItem.js
+++ src/components/PlayListItem.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
 export default class PlayListItem extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    let songInfo = this.props.songs.map((songs, files) => {
+      <li key={files} className="songInfo">
+        <div>{songs.userName}</div>
+        <div>{songs.songArtist}</div>
+        <div>{songs.songTitle}</div>
+        <div>{songs.songNotes}</div>
+      </li>
+    }
+  )
     return (
       <div className="pl-item">
-      <div>{this.props.songs.userName}</div>
-      <div>songArtist</div>
-      <div>songTitle</div>
-      <div>songNotes</div>
+        <ul>
+        {songInfo}
+        </ul>
       </div>
     )
   }
