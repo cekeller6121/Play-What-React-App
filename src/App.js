558334bdbeb816a368c3deddcfc68dfa141c9aa9
chadwick <cekeller6121@yahoo.com>
2017-07-26 18:24:37 -0400

    move css files into styles folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
 
 import NavBar from './components/NavBar.js';
 import PlayList from './components/PlayList.js';
