98593f76781053031c79342279cd87adfa25d1dc
chadwick <cekeller6121@yahoo.com>
2017-07-27 13:33:04 -0400

    add post code to playlistform

--- src/components/PlayListForm.js
+++ src/components/PlayListForm.js
@@ -5,7 +5,27 @@ export default class PlayListForm extends Component {
     super();
     this.state = {};
   }
-  
+  addToList = (e) => {
+    e.preventDefault();
+    this.setState({userName: e.target.value, songTitle: e.target.value, songArtist: e.target.value, songNotes: e.target.value});
+    let listItem = JSON.stringify(this.state);
+
+    fetch("https://tiny-lasagna-server.herokuapp.com/collections/playlisting", {
+      method: "POST",
+      body: listItem,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+  }
+  ).then(response => {
+    console.log(response, "yay");
+
+  }).catch(err => {
+    console.log(err, "boo!");
+  });
+  this.setState({userName: '', songNotes: '', songArtist: '', songTitle:''});
+}
   render() {
     return (
     <div className="formForPlayList"><form>
@@ -22,25 +42,3 @@ export default class PlayListForm extends Component {
     )
   }
 }
-
-// addToList = (e) => {
-//     e.preventDefault();
-//     this.setState({userName: e.target.value, songTitle: e.target.value, songArtist: e.target.value, songNotes: e.target.value});
-//     let listItem = JSON.stringify(this.state);
-//
-//     fetch("https://tiny-lasagna-server.herokuapp.com/collections/playlisting", {
-//       method: "POST",
-//       body: listItem,
-//       headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json'
-//     }
-//   }
-//   ).then(response => {
-//     console.log(response, "yay");
-//
-//   }).catch(err => {
-//     console.log(err, "boo!");
-//   });
-//   this.setState({userName: '', songNotes: '', songArtist: '', songTitle:''});
-// }
